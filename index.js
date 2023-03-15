83105bb962e88532776757c47d8065ef6527812c
hugocoding21 <hhanocq@gmail.com>
2023-03-15 14:05:44 +0100

    commit 2

---
+++ index.js
@@ -0,0 +1,11 @@
+const conteur = 0;
+const btn1 = document.querySelector("button");
+const click = document.querySelector(".compteur");
+compteur.innerHTML = compteur.addEventListener("click", function (event) {
+  click.innerHTML = "conteur+ cookies";
+});
+
+Button.addEventListener("click", function () {
+  conteur++;
+  conteur.innerHTML = `${conteur} cookie`;
+});
