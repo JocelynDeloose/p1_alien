b283a40db9220ebffc16c97d7c18dd2202fa5f02
Anthony Senechal <anthony.senechal59128@gmail.com>
2023-03-17 17:24:11 +0100

    commit fin de semaine

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ alien.onclick = function (nombreClics) {
   nombreClics.innerHTML = compteur++;
 };
 
+const dialog =
+document.getElementById('dialog');
+const openBtn =
+document.getElementById('openBtn');
+const closeBtn =
+document.getElementById('closeBtn');
+
+openBtn.addEventListener('click',
+function () {
+  dialog.setAttribute('open', true);
+})
+
+closeBtn.addEventListener('click',
+function () {
+  dialog.removeAttribute('open')
+});
